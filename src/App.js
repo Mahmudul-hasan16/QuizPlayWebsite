c38c3b611771facc4e19c235956b87f599b52582
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-10-13 13:41:54 +0600

    components connected and nav&header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Header from './components/Header/Header';
+import Quiz from './components/Quiz/Quiz';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layout/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Header />,
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz')
+      },
+      {
+        path: 'topics',
+        element: <Topics></Topics>,
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz')
+      },
+      {
+        path: '/topic/:topicId',
+        loader: async ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.topicId}`),
+        element: <Quiz />
+      },
+      {
+        path: 'statistics',
+        loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+        element: <Statistics />
+      },
+      {
+        path: 'blog',
+        element: <Blog />
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-center" autoClose={1000} />
+    </>
+
   );
 }
 
