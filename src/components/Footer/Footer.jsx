ab8c7b2311fcaabcbc13ef4d9db7ad2d40d95733
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-10-13 23:59:26 +0600

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='text-center p-16 bg-gray-800 '>
-            <h3 className='text-2xl font-bold'>Quiz <span className='bg-blue-500 text-white p-1 rounded'>mania</span> </h3>
-            <p className='text-white mt-2'>© All right reserved by Md Soliman Alam Soad</p>
+            <h3 className='text-2xl font-bold'>Quiz <span className='bg-blue-500 text-white p-1 rounded'>Play</span> </h3>
+            <p className='text-white mt-2'>© All right reserved by Md. Mahmudul Hasan</p>
         </div>
     );
 };
